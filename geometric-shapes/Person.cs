705183578e8b55b9d995119350a1f57e7a4310c6
Jacob Witte <wittejg@gmail.com>
2023-02-15 11:54:36 -0500

    Add project files.

---
+++ geometric-shapes/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric_shapes
+{
+    internal class Person
+    {
+
+        public static string Fullname(string firstname, string lastname)
+        {
+            return $"{firstname} {lastname}";
+        }
+        public static string RevFullname(string firstname, string lastname)
+        {
+            return $"{lastname} {firstname}";
+        }
+
+    }
+}
---
+++ geometric-shapes/Program.cs
@@ -0,0 +1,33 @@
+﻿using geometric_shapes;
+using System;
+
+
+Sqr S1 = new Sqr(5);
+
+Console.WriteLine($"Perimeter is {S1.Perimeter()} and Area is {S1.Area()}");
+
+Rect R1 = new Rect(3, 5);
+
+Console.WriteLine($"Perimeter is {R1.Perimeter()} and Area is {R1.Area()}");
+
+Quad Q1 = new Quad(3, 4, 5, 6);
+
+Console.WriteLine($"Perimeter is {Q1.Perimeter()}");
+
+
+
+
+
+
+
+
+//var fullname = Person.Fullname("Jake", "Witte");
+//Console.WriteLine(fullname);
+//var revfullname = Person.RevFullname(lastname: "Jacob", firstname: "Witte");
+//Console.WriteLine(revfullname);
+//Person jake = new();
+
+//Console.WriteLine($"15 plus 3 equals {MathLib.Add(15, 3)}");
+//Console.WriteLine($"15 minus 3 equals {MathLib.Subtract(15, 3)}");
+//Console.WriteLine($"15 times 3 equals {MathLib.Multiply(15, 3)}");
+//Console.WriteLine($"15 divided by 0 equals {MathLib.Divide(15, 0)}");
